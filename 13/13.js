8ef54291c976b79ee0fd113475473bdcb71df019
Kateryna <kateryna.bohatova@gmail.com>
2018-12-05 22:44:03 +0200

    Add HomeWork 13 Task 1, 2

---
+++ 13/13.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+
+
+// fs.writeFile('views/13.pug','Welcome to', (err, data) => {
+//     console.log('Асинхронный метод записи файла');
+//     if(err) throw err;
+//
+// });
+
+fs.appendFile('13/13.pug', '\n Hello Node', (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+});
+
+fs.readFile('13/13.pug', 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
